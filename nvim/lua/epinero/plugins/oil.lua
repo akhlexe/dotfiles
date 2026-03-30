6bd24260ebf6e5324c40dd47c9ded07cb86e5f7a
Exequiel Pinero <exe.pine@gmail.com>
2026-03-30 22:18:27 +0000

    feat: adding initial openjson.json file and update in oil.lua nvim plugin

--- nvim/lua/epinero/plugins/oil.lua
+++ nvim/lua/epinero/plugins/oil.lua
@@ -1,8 +1,36 @@
 return {
-  "stevearc/oil.nvim",
-  dependencies = {"nvim-tree/nvim-web-devicons"},
-  opts = {},
-  keys = {
-    { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
-  }
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+
+	opts = {
+		view_options = {
+			show_hidden = true,
+		},
+
+		default_file_explorer = true,
+
+		columns = {
+			"icon",
+			-- "permissions",
+			-- "size",
+			-- "mtime",
+		},
+
+		keymaps = {
+			["<CR>"] = "actions.select",
+			["-"] = "actions.parent",
+			["_"] = "actions.open_cwd",
+			["g."] = "actions.toggle_hidden",
+			["<C-v>"] = "actions.select_vsplit",
+			["<C-s>"] = "actions.select_split",
+			["<C-p>"] = "actions.preview",
+			["q"] = "actions.close",
+		},
+
+		use_default_keymaps = false, -- since we define our own
+	},
+
+	keys = {
+		{ "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+	},
 }
