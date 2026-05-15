ab7a3e9ffe7e8b5c5018dc145e79ba9d21cdf864
Exequiel Piñero <exe.pine@gmail.com>
2026-05-15 17:21:28 +0000

    feat: adding ts_ls and eslint lsp

--- nvim/lua/epinero/lsp/servers/init.lua
+++ nvim/lua/epinero/lsp/servers/init.lua
@@ -3,11 +3,13 @@ return {
 	cssls = require("epinero.lsp.servers.cssls"),
 	docker_compose_language_service = require("epinero.lsp.servers.docker_compose_language_service"),
 	dockerls = require("epinero.lsp.servers.dockerls"),
+	eslint = require("epinero.lsp.servers.eslint"),
 	gopls = require("epinero.lsp.servers.gopls"),
 	html = require("epinero.lsp.servers.html"),
 	jsonls = require("epinero.lsp.servers.jsonls"),
 	lua_ls = require("epinero.lsp.servers.lua_ls"),
 	roslyn_ls = require("epinero.lsp.servers.roslyn_ls"),
 	sqlls = require("epinero.lsp.servers.sqlls"),
+	ts_ls = require("epinero.lsp.servers.ts_ls"),
 	yamlls = require("epinero.lsp.servers.yamlls"),
 }
