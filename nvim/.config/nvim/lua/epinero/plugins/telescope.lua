180ffdc1237b4a5d2068edec49588b027eac9eae
Exequiel Piñero <exe.pine@gmail.com>
2026-07-01 12:27:26 +0000

    feat(nvim): removing search buffers keybind

--- nvim/.config/nvim/lua/epinero/plugins/telescope.lua
+++ nvim/.config/nvim/lua/epinero/plugins/telescope.lua
@@ -89,7 +89,6 @@ return {
 		pcall(require("telescope").load_extension, "ui-select")
 
 		-- Core Keymaps
-		vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]earch [B]uffers" })
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
