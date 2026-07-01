837f4a436af6ce7d3c544dfca85898ed2e2f5ae4
Exequiel Pinero <exe.pine@gmail.com>
2026-07-01 23:39:35 +0000

    feat(which-keys): updating group names

--- nvim/.config/nvim/lua/epinero/plugins/which-keys.lua
+++ nvim/.config/nvim/lua/epinero/plugins/which-keys.lua
@@ -2,9 +2,16 @@ return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
 	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
+		spec = {
+			{ "<leader>c", group = "Code" },
+			{ "<leader>d", group = "Debug" },
+			{ "<leader>f", group = "Find/Format" },
+			{ "<leader>g", group = "Git" },
+			{ "<leader>r", group = "Refactor" },
+			{ "<leader>s", group = "Search" },
+			{ "<leader>t", group = "Tabs/Toggle" },
+			{ "<leader>w", group = "Workspace" },
+		},
 	},
 	keys = {
 		{
