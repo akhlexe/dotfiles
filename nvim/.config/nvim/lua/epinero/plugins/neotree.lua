a819e8329758f6c63d949084de43c9dfe8ad9e34
Exequiel Piñero <exe.pine@gmail.com>
2026-07-01 15:34:50 +0000

    fix(neotree): use table form for follow_current_file in v3.x

--- nvim/.config/nvim/lua/epinero/plugins/neotree.lua
+++ nvim/.config/nvim/lua/epinero/plugins/neotree.lua
@@ -22,7 +22,7 @@ return {
         },
       },
       filesystem = {
-        follow_current_file = true,
+        follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
       },
     })
