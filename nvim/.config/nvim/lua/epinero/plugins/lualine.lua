c2d7590cbf1b46a10eee6bce5ede2a549a210dd1
Exequiel Piñero <exe.pine@gmail.com>
2026-07-01 20:17:50 +0000

    feat(theme): sinchronyzing theme for lualine with colorscheme

--- nvim/.config/nvim/lua/epinero/plugins/lualine.lua
+++ nvim/.config/nvim/lua/epinero/plugins/lualine.lua
@@ -38,7 +38,7 @@ return {
     require('lualine').setup {
       options = {
         icons_enabled = true,
-        theme = 'nord',
+        theme = 'auto',
         section_separators = { left = '', right = '' },
         component_separators = { left = '', right = '' },
         disabled_filetypes = { 'alpha', 'neo-tree'},
