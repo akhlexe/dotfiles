9b7e556682b68a9d4e58b52af353f9e865ab8d8f
Exequiel Piñero <exe.pine@gmail.com>
2026-07-02 17:22:00 +0000

    fix(dap-cs): fixing dap to run correctly and find correct workspace

--- nvim/.config/nvim/lua/epinero/dap/cs.lua
+++ nvim/.config/nvim/lua/epinero/dap/cs.lua
@@ -6,10 +6,25 @@ function M.setup(dap)
 			type = "coreclr",
 			name = "Launch .NET project",
 			request = "launch",
-			cwd = "${workspaceFolder}",
-			program = function()
-				return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
+			cwd = function()
+				local pattern = vim.fn.getcwd() .. "/src/**/bin/Debug/net8.0/*.API.dll"
+				local files = vim.fn.glob(pattern, false, true)
+				if #files > 0 then
+					return vim.fn.fnamemodify(files[1], ":h:h:h:h")
+				end
+				return vim.fn.getcwd()
 			end,
+			program = function()
+				local pattern = vim.fn.getcwd() .. "/src/**/bin/Debug/net8.0/*.API.dll"
+				local files = vim.fn.glob(pattern, false, true)
+				if #files > 0 then
+					return files[1]
+				end
+				return vim.fn.input("Path to dll: ", "", "file")
+			end,
+			env = {
+				ASPNETCORE_ENVIRONMENT = "Development",
+			},
 		},
 	}
 end
