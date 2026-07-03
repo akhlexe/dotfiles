a0248c9b9a10b9122251715193e7ec500de18b9d
Exequiel Piñero <exe.pine@gmail.com>
2026-07-03 18:21:49 +0000

    fix(refactoring): update refactoring keybinds where info were not correct

--- nvim/.config/nvim/lua/epinero/plugins/refactoring.lua
+++ nvim/.config/nvim/lua/epinero/plugins/refactoring.lua
@@ -12,31 +12,31 @@ return {
 
 		-- keymaps
 		vim.keymap.set({ "n", "x" }, "<leader>re", function()
-			return require("refactoring").refactor("Extract Function")
-		end, { expr = true, desc = "Refactor: Extract function" })
+			refactoring.refactor("Extract Function")
+		end, { desc = "Refactor: Extract function" })
 
 		vim.keymap.set({ "n", "x" }, "<leader>rf", function()
-			return require("refactoring").refactor("Extract Function To File")
-		end, { expr = true, desc = "Refactor: Extract function to file" })
+			refactoring.refactor("Extract Function To File")
+		end, { desc = "Refactor: Extract function to file" })
 
 		vim.keymap.set({ "n", "x" }, "<leader>rv", function()
-			return require("refactoring").refactor("Extract Variable")
-		end, { expr = true, desc = "Refactor: Extract variable" })
+			refactoring.refactor("Extract Variable")
+		end, { desc = "Refactor: Extract variable" })
 
 		vim.keymap.set({ "n", "x" }, "<leader>rI", function()
-			return require("refactoring").refactor("Inline Function")
-		end, { expr = true, desc = "Refactor: Inline function" })
+			refactoring.refactor("Inline Function")
+		end, { desc = "Refactor: Inline function" })
 
 		vim.keymap.set({ "n", "x" }, "<leader>ri", function()
-			return require("refactoring").refactor("Inline Variable")
-		end, { expr = true, desc = "Refactor: Inline variable" })
+			refactoring.refactor("Inline Variable")
+		end, { desc = "Refactor: Inline variable" })
 
 		vim.keymap.set({ "n", "x" }, "<leader>rbb", function()
-			return require("refactoring").refactor("Extract Block")
-		end, { expr = true, desc = "Refactor: Extract block" })
+			refactoring.refactor("Extract Block")
+		end, { desc = "Refactor: Extract block" })
 
 		vim.keymap.set({ "n", "x" }, "<leader>rbf", function()
-			return require("refactoring").refactor("Extract Block To File")
-		end, { expr = true, desc = "Refactor: Extract block to file" })
+			refactoring.refactor("Extract Block To File")
+		end, { desc = "Refactor: Extract block to file" })
 	end,
 }
