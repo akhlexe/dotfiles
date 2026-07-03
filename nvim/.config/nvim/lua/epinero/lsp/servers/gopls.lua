4ba74701ec124b8f671fa70586ac514afdf5e4a3
Exequiel Piñero <exe.pine@gmail.com>
2026-07-03 20:13:32 +0000

    feat(gopls): improving gopls diagnostics + hints

--- nvim/.config/nvim/lua/epinero/lsp/servers/gopls.lua
+++ nvim/.config/nvim/lua/epinero/lsp/servers/gopls.lua
@@ -3,6 +3,18 @@ return {
 		gopls = {
 			analyses = {
 				unusedparams = true,
+				unusedvariable = true,
+				shadow = true,
+				unusedwrite = true,
+			},
+			hints = {
+				rangeVariableTypes = true,
+				parameterNames = true,
+				constantValues = true,
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+				functionTypeParameters = true,
 			},
 			staticcheck = true,
 			gofumpt = true,
